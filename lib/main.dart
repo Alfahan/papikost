b351efd306c732a823210643635451245bc2df78
alfahan <ali.farhan160@gmail.com>
2021-02-28 14:18:17 +0700

    Implement Real API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:papikost/pages/splash_page.dart';
+import 'package:papikost/providers/space_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashPage(),
+    return ChangeNotifierProvider(
+      create: (context) => SpaceProvider(),
+      child: MaterialApp(
+        home: SplashPage(),
+      ),
     );
   }
 }
