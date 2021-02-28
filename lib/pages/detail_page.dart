8a94f68388aa4410b5f2f5aa2bd5cec0dbc2b7cf
alfahan <ali.farhan160@gmail.com>
2021-02-28 12:50:52 +0700

    Done 1

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:papikost/theme.dart';
 import 'package:papikost/widgets/facilites_item.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'error_page.dart';
 
 class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    launchUrl(String url) async {
+      if (await canLaunch(url)) {
+        launch(url);
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ErrorPage(),
+          ),
+        );
+      }
+    }
+
     return Scaffold(
+      backgroundColor: whiteColor,
       body: SafeArea(
           bottom: false,
           child: Stack(
@@ -15,27 +31,6 @@ class DetailPage extends StatelessWidget {
                 width: MediaQuery.of(context).size.width,
                 height: 350,
                 fit: BoxFit.cover,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: edge, vertical: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Image.asset(
-                        'assets/btn_back.png',
-                        width: 40,
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/btn_wishlist.png',
-                      width: 40,
-                    ),
-                  ],
-                ),
               ),
               ListView(
                 children: [
@@ -155,12 +150,150 @@ class DetailPage extends StatelessWidget {
                               ),
                             ],
                           ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        // NOTE PHOTO
+                        Padding(
+                          padding: EdgeInsets.only(left: edge),
+                          child: Text(
+                            'Photos',
+                            style: regularTextStyle.copyWith(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Container(
+                          height: 88,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              SizedBox(
+                                width: edge,
+                              ),
+                              Image.asset(
+                                'assets/pic (3).png',
+                                width: 110,
+                                height: 88,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(
+                                width: 18,
+                              ),
+                              Image.asset(
+                                'assets/pic (4).png',
+                                width: 110,
+                                height: 88,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(
+                                width: 18,
+                              ),
+                              Image.asset(
+                                'assets/pic (5).png',
+                                width: 110,
+                                height: 88,
+                                fit: BoxFit.cover,
+                              ),
+                              SizedBox(
+                                width: 18,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        // NOTE Location
+                        Padding(
+                          padding: EdgeInsets.only(left: edge),
+                          child: Text(
+                            'Location',
+                            style: regularTextStyle.copyWith(fontSize: 16),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: edge),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Jln. Kawan Sukses No.20\nPalembang",
+                                style: greyTextStyle,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  // launchUrl(
+                                  //     'https://goo.gl/maps/3R4L2SAuu45j8UwaA');
+
+                                  launchUrl('qwerty');
+                                },
+                                child: Image.asset(
+                                  'assets/location.png',
+                                  width: 40,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: edge,
+                          ),
+                          height: 50,
+                          width: MediaQuery.of(context).size.width - (2 * edge),
+                          child: RaisedButton(
+                            onPressed: () {
+                              launchUrl('tel:+628993613408');
+                            },
+                            color: purpleColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(17)),
+                            child: Text(
+                              'Book Now',
+                              style: whiteTextStyle.copyWith(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 40,
                         )
                       ],
                     ),
                   ),
                 ],
-              )
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: edge, vertical: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Image.asset(
+                        'assets/btn_back.png',
+                        width: 40,
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/btn_wishlist.png',
+                      width: 40,
+                    ),
+                  ],
+                ),
+              ),
             ],
           )),
     );
